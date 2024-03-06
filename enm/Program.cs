7d5d36a9d7da2155d0ddfa4bc84a1f508b341af2
haider12 <haiderkareem196@gmail.com>
2024-03-06 11:32:04 +0300

    Loading

---
+++ enm/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+haider Haider = new haider();
+Haider.fullName = "Haider kareem";
+Haider.Age = 26;
+Haider.Country = Country.Bagdad;
+
+Console.WriteLine($"{Haider.fullName}, {Haider.Age}, {Haider.Country}") ;
+
+
+
+
+haider Haider1=new haider("haider2",66,Country.spain);
+Console.WriteLine($"{Haider1.fullName}, {Haider1.Age}, {Haider1.Country}");
+
+// استدعاء عادي
+
+Console.WriteLine(Country.canada);
+public enum Country
+{
+    Bagdad,
+    Egypt,
+    Usa,
+    canada,
+    spain,
+}
+
+
+ public class haider
+{
+    public string fullName { get; set; }
+    public int Age { get; set; }
+    public Country Country { get; set; }
+
+    public haider() { }
+
+    public haider(string ful,int ag,Country cou) 
+    { 
+        fullName = ful;
+        Age = ag;
+        Country = cou;
+    }
+
+}
